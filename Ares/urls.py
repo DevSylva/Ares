7337be1894f8b1ddc2dcc6b29c275c485483bf7b
sylva <sylvaejike@gmail.com>
2022-04-15 03:33:20 +0100

    update

--- Ares/urls.py
+++ Ares/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,3 +15,8 @@ urlpatterns = [
     path('core/', include('core.urls')),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
