9ecba4e160064151c72561e2301b4d1df99db9c4
sylva <sylvaejike@gmail.com>
2022-06-17 14:02:11 +0100

    'oo'

--- core/views.py
+++ core/views.py
@@ -87,7 +87,7 @@ def deposit(request):
             try:
                 data = {
                     "subject": "Incoming Payment",
-                    "body": "Hello boss, a payment of ${} has been initialized by {},\n Go check it out and approve".format(request.POST['amount'], request.user)
+                    "body": "Hello boss, a payment of ${} has been initialized by {},confirm and approve payment.".format(request.POST['amount'], request.user)
                 }
                 Util.send_email(data)
                 print("email has been successfully sent!")
