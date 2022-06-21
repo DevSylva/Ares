ee0fd571fe150d12743d00095db64cddc3dea93d
sylva <sylvaejike@gmail.com>
2022-06-21 21:12:08 +0100

    safdf

--- account/admin.py
+++ account/admin.py
@@ -5,6 +5,6 @@ from .models import User
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "account_balance", "alaye")
-    list_filter = ("username", "email", "gender")
+    list_filter = ("username", "email", "alaye")
 
 admin.site.register(User, UserAdmin)
